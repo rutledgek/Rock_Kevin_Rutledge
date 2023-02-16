75763b028e8e79fe9993adf7fd1b1661e9d9750c
Melanie Spence <45180558+melzs0627@users.noreply.github.com>
2023-02-16 14:36:02 -0700

    - Update formatting for Group Type filters

--- Rock/Reporting/DataFilter/Person/GroupTypeFirstAttendanceFilter.cs
+++ Rock/Reporting/DataFilter/Person/GroupTypeFirstAttendanceFilter.cs
@@ -114,7 +114,7 @@ namespace Rock.Reporting.DataFilter.Person
                 }
 
                 filterName = string.Format(
-                    "Attended '{0}'{4} {1} {2} times. Date Range: {3}",
+                    "Attended '{0}' Group Type {4} {1} {2} times. Date Range: {3}",
                     groupType ?? "?",
                     comparisonType.ConvertToString(),
                     options[2],
@@ -140,7 +140,7 @@ namespace Rock.Reporting.DataFilter.Person
             var cbChildGroupTypes = new RockCheckBox();
             cbChildGroupTypes.ID = filterControl.ID + "_cbChildGroupTypes";
             cbChildGroupTypes.AddCssClass( "js-child-group-types" );
-            cbChildGroupTypes.Text = "Include Child Group Types(s)";
+            cbChildGroupTypes.Text = "Include Child Group Type(s)";
             filterControl.Controls.Add( cbChildGroupTypes );
 
             var ddlIntegerCompare = ComparisonHelper.ComparisonControl( ComparisonHelper.NumericFilterComparisonTypes );
