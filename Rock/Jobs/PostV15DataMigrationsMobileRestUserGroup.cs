a6e9a4eedff07dc77f8790acbd74d87d7fc5959f
braden <braden.cohen@Yahoo.com>
2023-03-07 14:55:03 -0700

    - general code cleanup

--- Rock/Jobs/PostV15DataMigrationsMobileRestUserGroup.cs
+++ Rock/Jobs/PostV15DataMigrationsMobileRestUserGroup.cs
@@ -24,8 +24,7 @@ namespace Rock.Jobs
             {
                 var personService = new PersonService( rockContext );
                 var groupService = new GroupService( rockContext );
-                var userLoginService = new UserLoginService( rockContext );
-
+                
                 var mobileAppUsersGuid = SystemGuid.Group.GROUP_MOBILE_APPLICATION_USERS.AsGuid();
                 var group = groupService
                     .Queryable()
@@ -36,62 +35,106 @@ namespace Rock.Jobs
                 // Create the rest security group if it doesn't exist.
                 if ( group == null )
                 {
-                    group = new Group
-                    {
-                        Guid = SystemGuid.Group.GROUP_MOBILE_APPLICATION_USERS.AsGuid(),
-                        IsSystem = true,
-                        ParentGroupId = null,
-                        GroupTypeId = 1,
-                        CampusId = null,
-                        Name = "RSR - Mobile Application Users",
-                        Description = "Group of mobile application people to use for endpoint authorization.",
-                        IsSecurityRole = true,
-                        IsActive = true,
-                        ElevatedSecurityLevel = Utility.Enums.ElevatedSecurityLevel.Extreme,
-                        IsPublic = true
-                    };
+                    group = GetDefaultMobileUsersGroup();
 
                     groupService.Add( group );
                     rockContext.SaveChanges();
                 }
 
-                // All of our mobile application sites.
-                var mobileSiteUserIds = SiteCache.GetAllActiveSites()
-                    .Where( s => s.SiteType == Rock.Model.SiteType.Mobile )
-                    .Select( s => s.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>()?.ApiKeyId )
-                    .Where( id => id.HasValue )
-                    .ToList();
+                var groupTypeId = GroupTypeCache.Get( group.GroupTypeId ).Id;
 
-                // Get all of the user logins associated with a mobile site.
-                var mobileSitePeopleIds = userLoginService.Queryable()
-                    .Where( ul => mobileSiteUserIds.Contains( ul.Id ) && ul.PersonId.HasValue )
-                    .Select( ul => ul.PersonId )
-                    .ToList();
-
-                var groupType = GroupTypeCache.Get( group.GroupTypeId );
-
-                // Add each mobile application user into the group.
-                foreach ( var personId in mobileSitePeopleIds )
-                {
-                    // Ensure there are no duplicates.
-                    if( !group.Members.Any( gm => gm.PersonId == personId ) )
-                    {
-                        
-                        GroupMember groupMember = new GroupMember();
-
-                        // We explicitly set the group here because if we don't,
-                        // the group will later be loaded from the database via
-                        // a pre-save hook.
-                        groupMember.Group = group;
-                        groupMember.GroupId = group.Id;
-                        groupMember.PersonId = personId.Value;
-                        groupMember.GroupRoleId = groupType?.DefaultGroupRoleId ?? 0;
-                        groupMember.GroupMemberStatus = GroupMemberStatus.Active;
-                        group.Members.Add( groupMember );
-                    }
-                }
-
+                // Ensure all of the mobile application are in this group.
+                EnsureAllMobileApplicationsAreInDefaultGroup( rockContext, group, groupTypeId );
                 rockContext.SaveChanges();
+            }
+
+            DeleteJob();
+        }
+
+        /// <summary>
+        /// Ensures all mobile application users are in the default group.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="group">The group.</param>
+        /// <param name="groupTypeId">The group type identifier.</param>
+        private void EnsureAllMobileApplicationsAreInDefaultGroup( RockContext rockContext, Group group, int groupTypeId )
+        {
+            var userLoginService = new UserLoginService( rockContext );
+
+            // A list of an 'ApiKeyId' object that every mobile application should contain.
+            var mobileSiteUserIds = SiteCache.GetAllActiveSites()
+                .Where( s => s.SiteType == Rock.Model.SiteType.Mobile )
+                .Select( s => s.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>()?.ApiKeyId )
+                .Where( id => id.HasValue )
+                .ToList();
+
+            // Get all of the user logins associated with a mobile site.
+            var mobileSitePeopleIds = userLoginService.Queryable()
+                .Where( ul => mobileSiteUserIds.Contains( ul.Id ) && ul.PersonId.HasValue )
+                .Select( ul => ul.PersonId )
+                .ToList();
+
+            var groupType = GroupTypeCache.Get( groupTypeId );
+
+            // Add each mobile application user into the group.
+            foreach ( var personId in mobileSitePeopleIds )
+            {
+                // Ensure there are no duplicates.
+                if ( !group.Members.Any( gm => gm.PersonId == personId ) )
+                {
+
+                    GroupMember groupMember = new GroupMember();
+
+                    // We explicitly set the group here because if we don't,
+                    // the group will later be loaded from the database via
+                    // a pre-save hook.
+                    groupMember.Group = group;
+                    groupMember.GroupId = group.Id;
+                    groupMember.PersonId = personId.Value;
+                    groupMember.GroupRoleId = groupType?.DefaultGroupRoleId ?? 0;
+                    groupMember.GroupMemberStatus = GroupMemberStatus.Active;
+                    group.Members.Add( groupMember );
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the default settings for creation of the 'Mobile Application Users' security group.
+        /// </summary>
+        /// <returns>Group.</returns>
+        private Group GetDefaultMobileUsersGroup()
+        {
+            return new Group
+            {
+                Guid = SystemGuid.Group.GROUP_MOBILE_APPLICATION_USERS.AsGuid(),
+                IsSystem = true,
+                ParentGroupId = null,
+                GroupTypeId = 1,
+                CampusId = null,
+                Name = "RSR - Mobile Application Users",
+                Description = "Group of mobile application people to use for endpoint authorization.",
+                IsSecurityRole = true,
+                IsActive = true,
+                ElevatedSecurityLevel = Utility.Enums.ElevatedSecurityLevel.Extreme,
+                IsPublic = true
+            };
+        }
+
+        /// <summary>
+        /// Deletes the job.
+        /// </summary>
+        private void DeleteJob()
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var jobService = new ServiceJobService( rockContext );
+                var job = jobService.Get( GetJobId() );
+
+                if ( job != null )
+                {
+                    jobService.Delete( job );
+                    rockContext.SaveChanges();
+                }
             }
         }
     }
