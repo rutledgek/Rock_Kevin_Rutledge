29b0564ec660e25e65046edfcf7ab2de1e8128d9
Melanie Spence <45180558+melzs0627@users.noreply.github.com>
2023-03-07 11:13:05 -0700

    - Update SystemConfiguration and web.config to add SignalR Endpoint and Access Key Fields

--- RockWeb/Blocks/Administration/SystemConfiguration.ascx.cs
+++ RockWeb/Blocks/Administration/SystemConfiguration.ascx.cs
@@ -187,6 +187,8 @@ namespace RockWeb.Blocks.Administration
             Configuration rockWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration( "~" );
             rockWebConfig.AppSettings.Settings["OrgTimeZone"].Value = ddTimeZone.SelectedValue;
             rockWebConfig.AppSettings.Settings["RunJobsInIISContext"].Value = cbRunJobsInIISContext.Checked.ToString();
+            rockWebConfig.AppSettings.Settings["AzureSignalREndpoint"].Value = rtbAzureSignalREndpoint.Text;
+            rockWebConfig.AppSettings.Settings["AzureSignalRAccessKey"].Value = rtbAzureSignalRAccessKey.Text;
 
             var section = ( System.Web.Configuration.SystemWebSectionGroup ) rockWebConfig.GetSectionGroup( "system.web" );
             section.HttpRuntime.MaxRequestLength = int.Parse( numbMaxSize.Text ) * 1024;
