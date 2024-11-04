f5185a813f08cdbf254333eda901564ae7b25646
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-04 15:16:48 -0500

    - Add Page System Guids for Migrations inlcuding already installed Installed Plugins Page.

--- online.kevinrutledge.InvoiceSystem/SystemGuids/SystemGuids.cs
+++ online.kevinrutledge.InvoiceSystem/SystemGuids/SystemGuids.cs
@@ -9,4 +9,10 @@ namespace online.kevinrutledge.InvoiceSystem.SystemGuids
         public const string Invoice_Type = "c60f47dc-8aa4-4468-a18e-5ff03bb00efd";
 
     }
+
+    public static class PageGuids
+    {
+        public const string InstalledPluginsPage = "5B6DBC42-8B03-4D15-8D92-AAFA28FD8616";
+        public const string InvoiceTypeListPage = "92C183F8-C5A1-4EEC-AFC2-B1918662B5A7";
+    }
 }
