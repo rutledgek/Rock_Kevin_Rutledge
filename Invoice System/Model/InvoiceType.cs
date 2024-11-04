c64aeec35c3b9107164baac6c2a9c44890cfd1ab
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-04 12:36:36 -0500

    - Added Invoice Type Model and Service

---
+++ Invoice System/Model/InvoiceType.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Runtime.Serialization;
+
+using Rock.Data;
+using Rock.Model;
+
+namespace online.kevinrutledge.InvoiceSystem.Model
+{
+    [Table("_online_kevinrutledge_InvoiceSystem_InvoiceType")]
+    // That line goes right above the class definition...
+    [DataContract]
+    public class InvoiceType : Model<InvoiceType>, IRockEntity
+    {
+            /// <summary>
+            /// Gets or sets the name of the Invoice Type.
+            /// </summary>
+            [DataMember]
+            [Required(ErrorMessage = "Name is required")]
+            [MaxLength(100)]
+            public string Name { get; set; }
+
+            /// <summary>
+            /// Gets or sets a description of the Invoice Type.
+            /// </summary>
+            [DataMember]
+            public string Description { get; set; }
+
+            /// <summary>
+            /// Indicates whether this Invoice Type is active.
+            /// </summary>
+            [DataMember]
+            public bool IsActive { get; set; } = true;
+
+            /// <summary>
+            /// Gets or sets the term used to describe invoices of this type.
+            /// </summary>
+            [DataMember]
+            public string InvoiceTerm { get; set; }
+
+            /// <summary>
+            /// Gets or sets the term used to describe items on the invoice.
+            /// </summary>
+            [DataMember]
+            public string InvoiceItemTerm { get; set; }
+
+            /// <summary>
+            /// Gets or sets the prefix for financial batches related to this invoice type.
+            /// </summary>
+            [DataMember]
+            public string FinancialBatchPrefix { get; set; }
+
+            /// <summary>
+            /// Gets or sets the CSS class for an icon representing this invoice type.
+            /// </summary>
+            [DataMember]
+            public string IconCssClass { get; set; }
+
+            /// <summary>
+            /// Default communication template for invoices of this type.
+            /// </summary>
+            [DataMember]
+            public string DefaultCommunicationTemplate { get; set; }
+
+            /// <summary>
+            /// Communication template for late invoices.
+            /// </summary>
+            [DataMember]
+            public string LateInvoiceCommunicationTemplate { get; set; }
+
+            /// <summary>
+            /// Specifies the number of days after the due date that an invoice is considered late.
+            /// </summary>
+            [DataMember]
+            public int DaysUntilLate { get; set; }
+
+            /// <summary>
+            /// Default late fee amount for invoices of this type.
+            /// </summary>
+            [DataMember]
+            public decimal? LateFeeAmount { get; set; }
+
+            /// <summary>
+            /// Default late fee percentage for invoices of this type.
+            /// </summary>
+            [DataMember]
+            public decimal LateFeePercentage { get; set; }
+    }
+
+    public partial class InvoiceTypeConfiguration : EntityTypeConfiguration<InvoiceType>
+    {
+        public InvoiceTypeConfiguration()
+        {
+            // Specify relationships and cascade delete settings if any
+            // Since this example has no direct foreign keys on InvoiceType, 
+            // no specific foreign key configuration is set here. 
+
+            // Set entity set name for consistency and easy querying
+            this.HasEntitySetName("InvoiceType");
+        }
+    }
+}
---
+++ Invoice System/Model/InvoiceType_Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rock.Data;
+
+namespace online.kevinrutledge.InvoiceSystem.Model
+{
+    public class InvoiceTypeService : Service<InvoiceType>
+    {
+        public InvoiceTypeService(RockContext context) : base(context) { }
+
+        public bool CanDelete(InvoiceType item, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
