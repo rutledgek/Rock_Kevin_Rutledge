ad3918c7300ec3316abd8fa7d66264cc4f420bc5
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-04 13:20:31 -0500

    - Add Create a System Guids file for use in migrations to track all crated entities, pages, etc.

---
+++ Invoice System/SystemGuids/SystemGuids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+
+namespace online.kevinrutledge.InvoiceSystem.SystemGuids
+{
+    public static class EntityGuids
+    {
+        public const string Invoice_Type = "c60f47dc-8aa4-4468-a18e-5ff03bb00efd";
+
+    }
+}
