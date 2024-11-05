4f63132586fa13e26028ce2be141522451a08068
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-04 20:51:34 -0500

    - Add Invoice Type List Block that filters on campus and Including Incative.

---
+++ RockWeb/Plugins/online_kevinrutledge/InvoiceSystem/InvoiceTypeList.ascx
@@ -0,0 +1,20 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="InvoiceTypeList.ascx.cs" Inherits="RockWeb.Plugins.online_kevinrutledge.InvoiceSystem.InvoiceTypeList" %>
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+         <Rock:GridFilter ID="gfSettings" runat="server">
+            <Rock:CampusPicker ID="cpCampus" runat="server" Label="Campus" />
+            <Rock:RockCheckBox ID="cbShowInActive" runat="server" Label="Show Inactive" Checked="false"  />
+        </Rock:GridFilter>
+        <Rock:ModalAlert ID="mdGridWarning" runat="server" />
+
+        <Rock:Grid ID="gInvoiceTypes" runat="server" AllowSorting="true" OnRowSelected="gInvoiceTypes_Edit" TooltipField="Description">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Invoice Name" SortExpression="Name" />
+                <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+                <Rock:BoolField DataField="IsActive" HeaderText="Is Active" SortExpression="IsActe" />
+                <Rock:DeleteField OnClick="gInvoiceTypes_Delete" />
+            </Columns>
+        </Rock:Grid>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/online_kevinrutledge/InvoiceSystem/InvoiceTypeList.ascx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+using online.kevinrutledge.InvoiceSystem.Model;
+
+
+namespace RockWeb.Plugins.online_kevinrutledge.InvoiceSystem
+{
+    [DisplayName("Invoice Type List")]
+    [Category("online_kevinrutledge > Invoice System")]
+    [Description("List of all Invoice Types.")]
+
+    [LinkedPage("Detail Page","",true,online.kevinrutledge.InvoiceSystem.SystemGuids.PageGuids.InvoiceTypeDetailpage)]
+    public partial class InvoiceTypeList : Rock.Web.UI.RockBlock
+    {
+
+        #region Base Control Methods
+
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            bool canEdit = IsUserAuthorized(Rock.Security.Authorization.EDIT);
+
+            gfSettings.ApplyFilterClick += gfSettings_ApplyFilterClick;
+            gInvoiceTypes.Actions.ShowAdd = canEdit;
+            gInvoiceTypes.IsDeleteEnabled = canEdit;
+            gInvoiceTypes.Actions.AddClick += gInvoiceTypes_Add;
+
+            gInvoiceTypes.RowItemText = "Invoice Type";
+            gInvoiceTypes.DataKeyNames = new string[] { "id" };
+
+            BindFilter();
+        }
+
+        private void BindFilter()
+        {
+
+            bool showInactiveTypes = gfSettings.GetFilterPreference("Show Inactive Invoice Types").AsBoolean();
+
+            cbShowInActive.Checked = showInactiveTypes;
+
+            var campuses = CampusCache.All();
+            cpCampus.Campuses = campuses;
+            cpCampus.Visible = campuses.Any();
+
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!Page.IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+
+
+        private void BindGrid()
+        {
+            var service = new InvoiceTypeService(new RockContext());
+            SortProperty sortProperty = gInvoiceTypes.SortProperty;
+
+            var query = service.Queryable();
+
+            int? campusId = gfSettings.GetFilterPreference("Campus").AsIntegerOrNull();
+            if (campusId.HasValue)
+            {
+                query = query.Where(a => a.CampusId == campusId.Value);
+            }
+
+            bool showInactiveTypes = gfSettings.GetFilterPreference("Show Inactive Invoice Types").AsBoolean();
+
+            if (!showInactiveTypes)
+            {
+                query = query.Where(a => a.IsActive == true);
+            }
+
+
+            // Sort Results (For adding filters and sorts later)
+            if ( sortProperty != null )
+            {
+                gInvoiceTypes.DataSource = query.Sort(sortProperty).ToList();
+            }
+            else
+            {
+                gInvoiceTypes.DataSource = query.OrderBy(a => a.Name).ToList();
+
+            }
+
+            gInvoiceTypes.DataBind();
+        }
+
+        protected void gfSettings_ApplyFilterClick(object sender, EventArgs e)
+        {
+            var gfSettingsShowInactive = gfSettings.GetFilterPreference("Show Inactive Invoice Types");
+
+            gfSettings.SetFilterPreference("Campus", cpCampus.SelectedCampusId != null ? cpCampus.SelectedCampusId.Value.ToString() : string.Empty);
+            gfSettings.SetFilterPreference("Show Inactive Invoice Types", cbShowInActive.Checked.ToString());
+
+            BindGrid();
+        }
+
+        protected void gInvoiceTypes_Add(object sender, EventArgs e)
+        {
+            NavigateToDetailPage(0);
+        }
+
+        protected void gInvoiceTypes_Edit(object sender, RowEventArgs e)
+        {
+            NavigateToDetailPage(e.RowKeyId);
+        }
+
+        private void NavigateToDetailPage(int invoiceTypeId)
+        {
+            var queryParams = new Dictionary<string, string>();
+            queryParams.Add("invoiceTypeId", invoiceTypeId.ToString());
+
+            NavigateToLinkedPage("DetailPage", queryParams);
+        }
+
+
+
+        protected void gInvoiceTypes_Delete(object sender, RowEventArgs e)
+        {
+            var dataContext = new RockContext();
+            var service = new InvoiceTypeService(dataContext);
+            var invoiceType = service.Get((int)e.RowKeyValue);
+            if (invoiceType != null)
+            {
+                string errorMessage;
+                if (!service.CanDelete(invoiceType, out errorMessage))
+                {
+                    mdGridWarning.Show(errorMessage, ModalAlertType.Information);
+                    return;
+                }
+
+                service.Delete(invoiceType);
+                dataContext.SaveChanges();
+            }
+
+            BindGrid();
+        }
+
+
+        protected void gInvoiceTypes_GridRebind(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+        #endregion
+    }
+}
