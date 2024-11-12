32a68d2abb5c1ef948a0cca5b4248095340b52c5
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-12 16:13:47 -0500

    Create Invoice Entity Type

---
+++ online.kevinrutledge.InvoiceSystem/Model/InvoiceType_Service - Copy (2).cs
@@ -0,0 +1,17 @@
+﻿
+
+using Rock.Data;
+
+namespace online.kevinrutledge.InvoiceSystem.Model
+{
+    public class InvoiceTypeService : Service<InvoiceType>
+    {
+        public InvoiceTypeService(RockContext context) : base(context) { }
+
+        public bool CanDelete(InvoiceType item, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
