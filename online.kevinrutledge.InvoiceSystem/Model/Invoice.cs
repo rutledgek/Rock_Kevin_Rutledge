63892342674dad819eb68bbad55372075ac545ec
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-12 17:11:18 -0500

    Create Invoice Model

--- online.kevinrutledge.InvoiceSystem/Model/Invoice.cs
+++ online.kevinrutledge.InvoiceSystem/Model/Invoice.cs
@@ -38,6 +38,50 @@ namespace online.kevinrutledge.InvoiceSystem.Model
         [DataMember(IsRequired = true)]
         public int InvoiceTypeId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name/title of the invoice.
+        /// </summary>
+        [DataMember]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a summary or description for the invoice.
+        /// </summary>
+        [DataMember]
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the invoice.
+        /// </summary>
+        [DataMember]
+        public InvoiceStatus InvoiceStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the due date for the invoice.
+        /// </summary>
+        [DataMember]
+        public DateTime? DueDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of days after the due date when this invoice is considered late.
+        /// </summary>
+        [DataMember]
+        public int? LateDays { get; set; }
+
+        /// <summary>
+        /// Gets or sets the late date for this invoice, if applicable.
+        /// </summary>
+        [DataMember]
+        public DateTime? LateDate { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the collection of invoice items associated with this invoice.
+        /// </summary>
+        /// [LavaInclude]
+        /// public virtual ICollection<InvoiceItem> InvoiceItems { get; set; } = new List<InvoiceItem>();
+
 
 
         #region Virtual Properties
@@ -66,4 +110,19 @@ namespace online.kevinrutledge.InvoiceSystem.Model
             this.HasEntitySetName("Invoice");
         }
     }
+
+
+    #region Enumerations
+
+    public enum InvoiceStatus
+    {
+        Draft = 0,
+        Scheduled = 1,
+        Sent = 2,
+        Paid = 3,
+        Late = 4,
+        Canceled = 5
+    }
+
+    #endregion
 }
