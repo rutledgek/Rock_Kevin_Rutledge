5dc2bf0b74db946ba58e70f6fd972a76cb3b7614
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-16 13:49:47 -0500

    Init Invoice Item Mode, Service, and Migration

---
+++ online.kevinrutledge.InvoiceSystem/Migrations/003_CreateDB_InvoiceItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice_System.Migrations
+{
+    internal class Class1
+    {
+    }
+}
