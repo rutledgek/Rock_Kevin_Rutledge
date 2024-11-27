809b367d50c9e93b85cc03b8071a4f94c29dd4b7
rutledgek <kevin.m.rutledge@gmail.com>
2024-11-26 22:19:53 -0500

    Added Model for Invoice Assignments.

---
+++ online.kevinrutledge.InvoiceSystem/Model/InvoiceAssignment_Service.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Rock.Data;
+
+namespace online.kevinrutledge.InvoiceSystem.Model
+{
+    public class InvoiceAssignmentService : Service<InvoiceAssignment>
+    {
+        public InvoiceAssignmentService(RockContext context) : base(context) { }
+
+        public bool CanDelete(InvoiceAssignment item, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
