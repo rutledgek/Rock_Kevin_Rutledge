b2614cf8afc07ec70fa69b3f9b9f3497e03015db
rutledgek <kevin.m.rutledge@gmail.com>
2025-02-05 16:41:10 -0500

    Added missing ScheduledInvoiceAssignmentService.

---
+++ online.kevinrutledge.InvoiceSystem/Model/ScheduledInvoiceAssignment_Service.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using System.Linq;
+
+namespace online.kevinrutledge.InvoiceSystem.Model
+{
+    public class ScheduledInvoiceAssignmentService : Service<ScheduledInvoiceAssignment>
+    {
+       
+        public ScheduledInvoiceAssignmentService(RockContext context) : base(context) { }
+
+        public bool CanDelete(InvoiceAssignment item, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
